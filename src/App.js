c48d286c6caf2218c93fb75ca430d964b829d7da
Suad <suadcodes@gmail.com>
2022-06-09 17:51:53 +0100

    last attempt

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import AddTask from "./components/AddTask"
 
 const App=() => {
 
+  const[showAddTask, setShowAddTask]= useState(false)
+
   const[tasks,setTasks]= useState([
     {
       id:1,
@@ -34,6 +36,16 @@ const App=() => {
 
     ))
   }
+
+  const addTask = (task) => {
+   const id = Math.floor(Math.random()*1000)+1
+   
+   const newTask = { id, ...task}
+   
+   setTasks([...tasks,newTask])
+
+  }
+  
   const toggleReminder =(id)=> {
      
    setTasks(tasks.map((task) => task.id ===id
@@ -42,8 +54,10 @@ const App=() => {
 
   return (
     <div className="container">
-      <Header />
-      <AddTask/>
+      <Header onAdd={() => setShowAddTask(!showAddTask)}
+      showAdd={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask}/>}
       {tasks.length>0?<Tasks tasks={tasks} 
        onDelete={deleteTask} onToggle= {toggleReminder}   />:'No Tasks Available'}
 
